71c37eaa3c0a7fdf3599a9985ec7980ce5a60216
Lai Costa <laicosta@MacBook-Pro-de-Lai.local>
2025-07-21 23:26:38 -0300

    Implementado registro de uauário

--- conexao.php
+++ conexao.php
@@ -1,2 +1,9 @@
 <?php
 $mysqli = new mysqli("localhost", "root", "", "sys_notas");
+
+//verifica conexão
+if($mysqli->connect_error){
+    die ("Erro de conexão: " . $mysqli->connect_error);
+} else{
+    echo "Conectado!";
+}
